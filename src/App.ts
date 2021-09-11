fc3b71a7bb926c5cd75608d7aa9256e1c49d93b7
Nathan Huberty <nathan.huberty@orange.fr>
2021-09-11 20:02:22 +0200

    Create modal on click

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,15 @@
-import Livestorm from '@livestorm/plugin'
+import { Chat, Modal } from '@livestorm/plugin'
 
 export default function() {
-  Livestorm.Chat.Buttons.registerChatShareButton({
+  Chat.Buttons.registerChatShareButton({
     label: 'Share a Document',
     icon: 'file',
-    onClick: () => console.log('someone clicked this button')
+    onClick: () => {
+      Modal.showIframe({
+        template: '<p>{{ content }}</p>',
+        variables: { content: 'hello' },
+        onMessage: (message) => { }
+      })
+    }
   })
 }
