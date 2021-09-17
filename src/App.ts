7592da154ed5196fb752004efbdde7ff84839648
Nathan Huberty <nathan.huberty@orange.fr>
2021-09-17 14:24:15 +0200

    Trying to make AWS work.

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,21 @@
 import { Chat, Modal } from '@livestorm/plugin'
 
+const fileTransferTemplate = require('./file-transfer.html').default
+const fileDownloadTemplate = require('./file-download.html').default
+
 export default function() {
   Chat.Buttons.registerChatShareButton({
     label: 'Share a Document',
     icon: 'file',
     onClick: () => {
       Modal.showIframe({
-        template: '<p>{{ content }}</p>',
-        variables: { content: 'hello' },
-        onMessage: (message) => { }
+        template: fileTransferTemplate + fileDownloadTemplate,
+        onMessage: ({ fileUrl, fileName, fileSize }) => {
+          Chat.broadcast({
+            text: `Shared a file <${fileName}> ${fileSize}mo`,
+            html: `<a target="_blank" href="${fileUrl}" download class="btn btn-primary">Download</button> ${fileDownloadTemplate}`
+          })
+        }
       })
     }
   })
